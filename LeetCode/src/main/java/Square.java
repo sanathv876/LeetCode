a4b4d5ca1385552306a0086b67805d17a5d92433
sanathv876 <snat.venkat@gmail.com>
2024-11-28 11:37:22 -0700

    more projects

--- LeetCode/src/main/java/Square.java
+++ LeetCode/src/main/java/Square.java
@@ -1,7 +1,18 @@
+import java.lang.Math;
 public class Square {
     public boolean isPowerOfTwo(int n) {
-        if()
-
-
+        double num = (double) n;
+        double answer = Math.log10(n) / Math.log10(2);
+        if((int) answer == answer){
+            return true;
+        }
+        return false;
     }
+
+    public static void main(String[] args) {
+        Square s = new Square();
+        System.out.println(s.isPowerOfTwo(16));
+    }
+
+
 }
