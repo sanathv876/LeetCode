dae01374b46b47408625edd1502dea1a733cd02e
sanathv876 <snat.venkat@gmail.com>
2025-01-08 21:37:28 -0800

    Two other programs I forgot to commit

--- LeetCode/src/main/java/LengthOfLastWord.java
+++ LeetCode/src/main/java/LengthOfLastWord.java
@@ -1,15 +1,13 @@
-public class LengthofLastWord {
+public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         String[] words = s.split(" ");
-        return words[words.length-1].length();
-
-
+        return words[words.length - 1].length();
 
 
     }
 
     public static void main(String[] args) {
-        LengthofLastWord a =  new LengthofLastWord();
+        LengthOfLastWord a = new LengthOfLastWord();
         System.out.println(a.lengthOfLastWord("Hello World"));
     }
 }
--- LeetCode/src/main/java/Math.java
+++ LeetCode/src/main/java/Math.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 public class Math {
     public int subtractProductAndSum(int n) {
         String m = Integer.toString(n);
-        String[] digits = m.split("");
 
         int product = 1;
         int sum =0;
 
-        for(int i =0; i < digits.length; i ++){
-            product*=Integer.parseInt(digits[i]);
-            sum+=Integer.parseInt(digits[i]);
+        for(int i =0; i < m.length(); i ++){
+            int temp = Integer.parseInt(m.substring(i,i+1));
+            product *= temp;
+            sum += temp;
         }
 
         return product - sum;
@@ -17,6 +17,6 @@ public class Math {
 
     public static void main(String[] args) {
         Math m = new Math();
-        System.out.println(m.subtractProductAndSum(4421));
+        System.out.println(m.subtractProductAndSum(234));
     }
 }
